8be8d6472b9fc057e85cb0c3eb18b22b0f333c45
fahimsoft <fahim.akhtar@nexoventas.com>
2022-01-04 16:45:28 +0500

    update on special charnatar

--- B2CPortal/Scripts/Common.js
+++ B2CPortal/Scripts/Common.js
@@ -1820,7 +1820,7 @@ var paymenttype = {
 
 //----------------------------for special charctar--------------------------
 $(document).ready(function () {
-    $(document).on('keypress', '#removespecialchar', function (event) {
+    $(document).on('keypress', '.removespecialchar', function (event) {
         var regex = RegExp("^[a-zA-Z0-9]+$");
         var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
         if (!regex.test(key)) {
